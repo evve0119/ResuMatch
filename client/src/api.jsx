d357d6708c0d4fc7a4078b9e741fbce9c314bbf4
evve0119 <58882040+evve0119@users.noreply.github.com>
2025-04-04 10:44:44 +0800

    Update api.jsx
    change backend api endpoint

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
+  baseURL: 'https://resumatch-backend-hud2d3crdue2d3ad.eastus2-01.azurewebsites.net',
   // For local development, you can switch this:
-  baseURL: 'http://localhost:8080',
+  // baseURL: 'http://localhost:8080',
 });
 
 export default api;
