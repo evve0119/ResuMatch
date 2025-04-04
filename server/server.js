807d789d06c79b9a19d69e3c0933b70a1140e141
evve0119 <58882040+evve0119@users.noreply.github.com>
2025-04-04 17:07:13 +0800

    puppeteer update for azure environment updated

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(cors({
-  origin: 'https://purple-moss-034ab441e.6.azurestaticapps.net',
+  origin: ['https://purple-moss-034ab441e.6.azurestaticapps.net', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
